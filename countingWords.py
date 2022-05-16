02783c95a61fb8fb9b70ac9f4c19b715371bef42
TanishT <86164408+TanishT@users.noreply.github.com>
2022-05-15 17:51:30 -0700

    Add files via upload

---
+++ countingWords.py
@@ -0,0 +1,11 @@
+introString = input("Enter Here : ")
+wordCount = 1
+characterCount = 0
+for i in introString :
+      characterCount = characterCount
+      if(i==' '):
+            wordCount = wordCount + 1
+print("Number of words")
+print(wordCount)
+print("Number of characters")
+print(characterCount)
